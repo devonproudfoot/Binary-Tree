d13ff9ba99a11617fd3e3df7dd4959770ead7794
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-11-15 01:21:18 +0000

    Completed problem

--- lib/binarytree.rb
+++ lib/binarytree.rb
@@ -1,31 +1,47 @@
-class BinaryTree
+class Node
   attr_accessor :payload, :left, :right
 
-  def initialize(payload, left, right)
+  def initialize(payload, left=nil, right=nil)
     @payload = payload
     @left = left
     @right = right
   end
-
-  def sort(array)
-
-  end
-
 end
 
 class BTreeSort
-
   def sort(array)
+    root = Node.new(array.shift)
 
+    array.each do |value|
+      push_node(root, value)
+    end
+    
+    return traverse_tree(root)
   end
 
+  def push_node(node, value)
+    if value > node.payload
+      if node.right
+        push_node(node.right, value)
+      else
+        node.right = Node.new(value)
+      end
+    elsif value < node.payload
+      if node.left
+        push_node(node.left, value)
+      else
+        node.left = Node.new(value)
+      end
+    end
+  end
+
+  def traverse_tree(node, sorted_array=[])
+    return sorted_array unless node
+
+    traverse_tree(node.left, sorted_array) if node.left
+    sorted_array.push(node.payload)
+    traverse_tree(node.right, sorted_array) if node.right
+
+    return sorted_array
+  end
 end
-
-
-seven = BinaryTree.new(7, nil, nil)
-five  = BinaryTree.new(5, nil, nil)
-four  = BinaryTree.new(4, nil, nil)
-six   = BinaryTree.new(6, nil, seven)
-three = BinaryTree.new(3, nil, six)
-two   = BinaryTree.new(2, four, five)
-trunk = BinaryTree.new(1, two, three)
--- spec/btree_spec.rb
+++ spec/btree_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe BTreeSort, type: :model do
     it 'should order the array' do
       expected = [1, 4, 6, 7, 9, 10, 14]
       array = [7, 4, 9, 1, 6, 14, 10]
-      expect(BTreeSort.sort(array)).to eq expected
+
+      btree = BTreeSort.new
+      expect(btree.sort(array)).to eq(expected)
     end
   end
 end
